4aa81b3b216f47ba68b1efa07230f0cf1d8e375d
manikanta_matta <manikantabablu1999@gmail.com>
2022-12-04 23:03:49 -0600

    All Code changes

---
+++ README.md
@@ -0,0 +1,2 @@
+1. Follow this Article to install Tensorflow on MacOS M1/M2 Chip
+https://medium.com/mlearning-ai/install-tensorflow-on-mac-m1-m2-with-gpu-support-c404c6cfb580
---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request
+import pickle
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+
+app = Flask(__name__)
+
+def predict_sentiment_emotion(text):
+    with open('tokenizer.pickle', 'rb') as handle:
+        tokenizer = pickle.load(handle)
+    tw = tokenizer.texts_to_sequences([text])
+    tw = pad_sequences(tw,maxlen=200)
+    model = pickle.load(open('model.pkl','rb'))
+    prediction = int(model.predict(tw).round().item())
+    if prediction==0:
+        return "User is Happy About the Flight Journey"
+    else:
+        return "User is unhappy about the flight journey"
+
+
+
+@app.route("/", methods=['GET','POST'])
+def predict():
+    output=""
+    if request.method == "POST":
+       sentence = request.form.get("review")
+       output = predict_sentiment_emotion(sentence)
+    #    return "<h1>"+str(output)+"<h1>"
+    return render_template("main.html", data=output)
+
+
+if __name__ == "main":
+    app.run(debug=True)
